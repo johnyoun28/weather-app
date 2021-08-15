eaaa723dea9ca57a87577e23caaa9f96ab2e7252
John Youn <john@Johns-MacBook-Pro.local>
2021-08-15 03:30:40 -0700

    redux set up

---
+++ app/src/components/WeatherForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherForm = () => {
+    return (
+        <div>
+            <h1>
+                Testing
+            </h1>
+        </div>
+    )
+}
+
+export default WeatherForm
